01e083f9982b98faa1b616669696aef69db53f93
Renata Cruz <rena-cruz@hotmail.com>
2022-01-25 13:44:18 -0300

    Alterando o tema

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
+import React from 'react';
+import { useRouter } from 'next/router';
 import appConfig from './config.json';
-
-function GlobalStyle() {
-    return (
-        <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-}
 
 function Titulo(props) {
     const Tag = props.tag || 'h1';
@@ -52,7 +25,7 @@ function Titulo(props) {
 //    return (
 //        <div>
 //            <GlobalStyle />
-//            <Titulo Tag="h2">Boas vindas de volta!</Titulo>
+//            <Titulo Tag="h2">Bem-vindo de volta!</Titulo>
 //            <h2>Discord - Alura Matrix</h2>
 //        </div>
 //    )
@@ -61,16 +34,17 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'rena-cruz';
+    //const username = 'rena-cruz';
+    const [username, setUsername] = React.useState('rena-cruz');
+    const roteamento = useRouter();
 
     return (
         <>
-            <GlobalStyle />
             <Box
                 styleSheet={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center',
-                    backgroundColor: appConfig.theme.colors.primary[500],
-                    backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+                    //backgroundColor: appConfig.theme.colors.primary[500],
+                    backgroundImage: 'url(https://wallpapercave.com/wp/wp5723757.jpg)',
                     backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
                 }}
             >
@@ -86,23 +60,52 @@ export default function PaginaInicial() {
                         width: '100%', maxWidth: '700px',
                         borderRadius: '5px', padding: '32px', margin: '16px',
                         boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
-                        backgroundColor: appConfig.theme.colors.neutrals[700],
+                        backgroundColor: appConfig.theme.colors.neutrals[500],
                     }}
                 >
                     {/* Formulário */}
                     <Box
                         as="form"
+                        onSubmit={function (infosDoEvento) {
+                            infosDoEvento.preventDefault();
+                            console.log('Alguém submeteu o form');
+                            roteamento.push('/chat');
+                            //window.location.href = '/chat';
+                        }}
                         styleSheet={{
                             display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                             width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
                         }}
                     >
-                        <Titulo tag="h2">Boas vindas de volta!</Titulo>
+                        <Titulo tag="h2">Bem-vindo de volta!</Titulo>
                         <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                             {appConfig.name}
                         </Text>
 
+                        {/*<input
+                            type= "text"
+                            value= {username}
+                            onChange= {function (event){
+                                console.log('usuario digitou', event.target.value);
+                                //Onde ta o valor?
+                                const valor = event.target.value;                               
+                                //trocar o valor da variavel
+                                //atraves do React e avise quem precisa
+                                setUsername(valor);
+                            }}
+                        />*/}
+                        
+
                         <TextField
+                            value={username}
+                            onChange={function (event){
+                                console.log('usuario digitou', event.target.value);
+                                //Onde ta o valor?
+                                const valor = event.target.value;                               
+                                //trocar o valor da variavel
+                                //atraves do React e avise quem precisa
+                                setUsername(valor);
+                            }}
                             fullWidth
                             textFieldColors={{
                                 neutral: {
